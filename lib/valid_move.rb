d13f047e1375d61c7c9c00a626a49dc5d538b2ca
remarkable-markup-1310 <wonsae.kim@gmail.com>
2019-03-27 19:51:06 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  return false if index.between?(0, 8)
+  return false if !index.between?(0, 8)
+  
   if position_taken?(board, index)
     return false
   else
